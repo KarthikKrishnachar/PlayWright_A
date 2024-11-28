329fb6222cc148214cbf32d13524d8c6cbae77a7
KarthikKrishnachar <Karthik.vidic@gmail.com>
2024-11-28 10:25:28 +0530

    'cretedloginpage and updated login creds

--- tests/LoginToNaukri.spec.js
+++ tests/LoginToNaukri.spec.js
@@ -7,9 +7,27 @@ test('name of test', async ({ page }) => {
     await page.goto('https://www.naukri.com/');
 
 
-    // const loginButton_locator = page.locator('xpath=//*[@id="login_Layer"]');
-    const loginButton_locator = page.locator('xpath=//*[@id="login_Layer"]');
+    //Locators
+    const button_Login_Homepage = page.locator('xpath=//*[@id="login_Layer"]');
+    const textBox_LoginEmail = page.locator('xpath=//*[@placeholder="Enter your active Email ID / Username"]');
+    const textBox_LoginPassword = page.locator('xpath=//*[@placeholder="Enter your password"]');
 
-    await page.click('xpath=//*[@id="login_Layer"]');
+    const button_Login_CredentialPage = page.locator('xpath=//*[@placeholder="Enter your password"]');
+    //Locators
+
+    //TestData
+    var userName_email = 'karthik.krishnachar1991@gmail.com';
+    var userName_password = 'meganfoxx';
+    //Testdata
+
+    console.log(button_Login_Homepage.isVisible);
+
+    await button_Login_Homepage.click;
+    await textBox_LoginEmail.clear();
+    await textBox_LoginEmail.fill(userName_email);
+    await textBox_LoginPassword.clear();
+    await textBox_LoginPassword.fill(userName_password);
+    await button_Login_CredentialPage.click;
+
 
 });
